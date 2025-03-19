d52bc8b5d9b731000ceeba5c7dcc9e2e29dd8ea6
Emilyempa <emilypettersson@hotmail.com>
2025-03-19 09:19:02 +0100

    Add test to check handling of multiple of same letter and also a test to check error handling if input is special characters

--- wordle.test.js
+++ wordle.test.js
@@ -9,11 +9,12 @@ Teststrategi
 3. Testa att funktionen returnerar rätt resultat för en gissning som innehåller rätt bokstäver men på fel plats. KLAR
 4. Testa att funktionen returnerar felmeddelande för en gissning innehåller för många bokstäver. KLART
 5. Testa att funktionen returnerar felmeddelande för en gissning som är för kort. KLART
-6. Testa att funktionen returnerar rätt resultat för en gissning som innehåller flera av samma bokstav.
+6. Testa att funktionen returnerar rätt resultat för en gissning som innehåller flera av samma bokstav. KLAR
 7. Testa att metoden som gör om till upperCase funkar som det ska. GÖR DET REDAN I ANDRA TEST
 8. Testa att funktionen returnerar felmeddelande om det är tomma strängar eller whitespace som input. KLART
 9. Testa att funktionen returnerar en array med objekt. GÖR DET REDAN I ANDRA TEST
 10. Testa att funktionen returnerar en array med objekt som har samma ordning som i gissningen. GÖR DET REDAN I ANDRA TEST
+11. Testa att funktionen hanterar specialtecken på önskat sätt. KLAR
 */
 
 describe("matchGuess function", () => {
@@ -49,6 +50,28 @@ describe("matchGuess function", () => {
       { letter: "Y", result: "misplaced" },
     ]);
   });
+
+  test("should handle multiple of same letter correctly", () => {
+    const result = matchGuess("rraar", "hurry");
+    expect(result).toEqual([
+      { letter: 'R', result: 'misplaced' },
+      { letter: 'R', result: 'misplaced' },
+      { letter: 'A', result: 'incorrect' },
+      { letter: 'A', result: 'incorrect' },
+      { letter: 'R', result: 'incorrect' }
+    ]);
+  });
+
+  test("should remove all special characters from input", () => {
+    const result = matchGuess("hello!","hello");
+    expect(result).toEqual([
+      { letter: "H", result: "correct" },
+      { letter: "E", result: "correct" },
+      { letter: "L", result: "correct" },
+      { letter: "L", result: "correct" },
+      { letter: "O", result: "correct" },
+    ])
+  })
 });
 
 describe("matchGuess error handling functions", () => {
